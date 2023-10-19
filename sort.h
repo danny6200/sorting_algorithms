e10803c5260561cbd602639ba696ae1774a00d2a
danny6200 <ugookorodaniel620@gmail.com>
2023-10-19 14:59:41 +0100

    Add: required files for project

---
+++ sort.h
@@ -0,0 +1,27 @@
+#ifndef SORT_H
+#define SORT_H
+
+/* Header Files*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+
+void print_array(const int *array, size_t size);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
