5281ed51d1d201292eb5eabde0c5778122757266
danny6200 <ugookorodaniel620@gmail.com>
2023-10-20 09:08:26 -0700

    Modify: 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,6 +13,12 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, jMin;
 	int temp;
 
+	if (size < 2)
+	{
+		print_array(array, size);
+		return;
+	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		jMin = i;
