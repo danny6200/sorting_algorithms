5da93c309773738de8aa908a0db9a2c2004eb85d
danny6200 <ugookorodaniel620@gmail.com>
2023-10-20 01:01:08 -0700

    Modify: 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,22 +10,28 @@
 #include "sort.h"
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, jMin;
+	int temp;
+
+	if (!array || !size)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		jMin = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (a[j] < a[jMin])
+			if (array[j] < array[jMin])
 			{
 				jMin = j;
 			}
 		}
 		if (jMin != i)
 		{
-			swap(a[i], a[jMin]);
+			temp = array[i];
+			array[i] = array[jMin];
+			array[jMin] = temp;
 		}
+		print_array(array, size);
 	}
-}
-				
+}	
