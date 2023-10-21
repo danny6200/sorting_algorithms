37439998af67d6f05681a22ddee59d7bb46ad7ce
LusterPearl <Salamioluwatosin90@gmail.com>
2023-10-21 10:02:11 -0700

    updated

---
+++ 104-heap_sort.c
@@ -0,0 +1,61 @@
+#include "sort.h"
+
+/**
+ * sift_down - Performs a sift-down operation on the heap
+ * @array: The array to be sorted
+ * @size: The size of the array
+ * @root: The root of the heap
+ * @end: The end of the heap
+ */
+void sift_down(int *array, size_t size, size_t root, size_t end)
+{
+	size_t max, child;
+	int temp;
+
+	while ((root * 2 + 1) <= end)
+	{
+		child = root * 2 + 1;
+		max = root;
+
+
+		if (child + 1 <= end && array[child] < array[child + 1])
+			max = child + 1;
+
+		if (array[root] < array[max])
+		{
+			temp = array[root];
+			array[root] = array[max];
+			array[max] = temp;
+			print_array(array, size);
+			root = max;
+		}
+		else
+			break;
+	}
+}
+
+/**
+ * heap_sort - Sorts an array of integers in ascending order using Heaps
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void heap_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = (size / 2); i > 0; i--)
+		sift_down(array, size, i - 1, size - 1);
+
+	for (i = size - 1; i > 0; i--)
+	{
+		temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+		sift_down(array, size, 0, i - 1);
+	}
+}
--- sort.h
+++ sort.h
@@ -37,6 +37,8 @@ void quick_sort_recursive(int *array, int low, int high, size_t size);
 void counting_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t *left, listint_t *right);
+void heap_sort(int *array, size_t size);
 
 
 #endif
